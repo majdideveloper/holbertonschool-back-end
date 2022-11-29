1eaea152539e0d82da681234d814dd1666e4f22d
majdi aribi <majdi.developer@gmail.com>
2022-11-29 17:02:38 +0100

    Export to JSON File

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,6 +7,15 @@ import json
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
-    print(user)
     todos = requests.get(
         api_url + "todos", params={"userId": sys.argv[1]}).json()
+
+    dicti = {str(user.get("id")): [{"task": task.get("title"),
+                                    "completed": task.get("completed"),
+                                    "username": user.get(
+                                        "username")}for task in todos]}
+
+    file_json = sys.argv[1] + ".json"
+    with open(file_json, "w") as f:
+
+        json.dump(dicti, f)
