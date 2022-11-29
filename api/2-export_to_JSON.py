b06940df0b3e4e63946014562ae4ead0fc616c86
majdi aribi <majdi.developer@gmail.com>
2022-11-29 17:12:33 +0100

    Export to JSON File

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     todos = requests.get(
         api_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    dicti = {str(user.get("id")): [{"task": task.get("title"),
-                                    "completed": task.get("completed"),
-                                    "username": user.get(
-                                        "username")}for task in todos]}
+    dicti = {user.get("id"): [{"task": task.get("title"),
+                               "completed": task.get("completed"),
+                               "username": user.get(
+        "username")} for task in todos]}
 
     file_json = sys.argv[1] + ".json"
     with open(file_json, "w") as f:
