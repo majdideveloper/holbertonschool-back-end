0958e16e2fa201da8263352d306dbfa6442e8cab
majdi aribi <majdi.developer@gmail.com>
2022-11-29 17:47:12 +0100

    Export to JSON File all userrs

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,18 +7,20 @@ import sys
 
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
-    
-    users = requests.get(api_url + "users")
-   
-    
-    for user in users:
-        todos = requests.get(
-        api_url + "todos", params={"userId": user.get("id")}).json()
-        dicti = {user.get("id"): [{"task": task.get("title"),
-                               "completed": task.get("completed"),
-                               "username": user.get(
-        "username")} for task in todos]}
 
-    file_json = sys.argv[1] + ".json"
+    users = requests.get(api_url + "users").json()
+
+    dicti = {}
+    for user in users:
+        user_id = user.get("id")
+        todos = requests.get(
+            api_url + "todos", params={"userId": user_id}).json()
+
+        dicti[user.get("id")] = [{"task": task.get("title"),
+                                  "completed": task.get("completed"),
+                                  "username": user.get(
+            "username")} for task in todos]
+
+    file_json = "todo_all_employees.json"
     with open(file_json, "w") as f:
         json.dump(dicti, f)
